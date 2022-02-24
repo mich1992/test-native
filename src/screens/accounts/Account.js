c0c4f2034b3020dc807899cc5e3d6c2e5e186701
mich1992 <michaelcermeno@gmail.com>
2022-02-24 14:52:11 +0000

    changes

--- src/screens/accounts/Account.js
+++ src/screens/accounts/Account.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text } from 'react-native-web'
-import * as firebase from "firebase"
+import { onAuthStateChanged , getAuth} from "firebase/auth"
 import UserGuest from './screenChilds/UserGuest'
 import UserLogged from './screenChilds/UserLogged'
 
 export default function Account() {
     const [login, setLogin] = useState(null)
     useEffect(() => {
-        firebase.auth().onAuthStateChanged((user) => {
+        onAuthStateChanged((user) => {
             console.log(user)
             !user ? setLogin(false) : setLogin(true)
         })
